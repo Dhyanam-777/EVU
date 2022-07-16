60ce74a47b9ba6f1a81127c62c9f03d20885ddf7
Pri48 <107844033+Pri48@users.noreply.github.com>
2022-07-16 17:05:40 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -2,41 +2,34 @@
 using namespace std;
 class report_data
 {
-    float ch,tri,hdl,ldl;
 public:
-    float s_ch()
+    float ch,tri,hdl,ldl;
+    int age;
+    float data()
     {
-        cout<<"\n\n \t ENTER YOUR S.CHORESTROL (in mg/dl) : ";
+        cout<<"\n \t ENTER YOUR S.CHORESTROL (in mg/dl) : ";
         cin>>ch;
-    }
-    float s_tri()
-    {
         cout<<"\n   \t ENTER YOUR S.TRIGLYCERIDE(in mg/dl) : ";
         cin>>tri;
-    }
-    float s_hdl()
-    {
         cout<<"\n   \t ENTER YOUR S.HDL (in mg/dl) : ";
         cin>>hdl;
-    }
-    float s_ldl()
-    {
         cout<<"\n   \t ENTER YOUR S.LDL (in mg/dl) : ";
         cin>>ldl;
     }
+    int in_age()
+    {
+        cout<<"\n   \t ENTER YOUR AGE:";
+        cin>>age;
+    }
 };
-
+//class s_tri
 int main()
 {
-  
-
     cout<<"\n\n\t\t\t---------------------Welcome to your Diet Planner---------------------"<<endl;
     cout<<"\t\t\t(kindly enter in digits form only)"<<endl;
     report_data p1;
-    p1.s_ch();
-    p1.s_tri();
-    p1.s_hdl();
-    p1.s_ldl();
+    p1.in_age();
+    p1.data();
     cout<<"\n\n\n\n\t\t\tThank you for entering your data it is under processing"<<endl;
     cout<<"\t\t\tGetting your diet ready..."<<endl;
     return 0;

ed2bc12fd778de5fa11a4bbddd7bf3c5976b5b8f
Pri48 <107844033+Pri48@users.noreply.github.com>
2022-07-17 13:29:18 +0530

    semi final code updated

--- program.cpp
+++ program.cpp
@@ -80,23 +80,62 @@ public:
             cout<<"\t** OIL **: PALM OIL/ COCONUT OIL/ OLIVE OIL/ SUNFLOWER OIL/ SOYABEAN OIL/\n\t** DAIRY PRODUCTS** : DARK CHOCOLATE/ BUTTER/ CHEESE/ CREAM/ BUTTERMILK\n\t** NON-VEG ** : SARDINE(FISH)/ BEEF/ LAMB/ PORK\n\t** DRY FRUITS ** : ALMONDS/ WALNUTS/ PEANUT\n\t** MUST ** : OATS/ AVACADO/ MILLET/ CHICKPEA/ GREEN-PEA"<<endl;
         }
     }
+        void diet1()
+    {
+        cout<<"\n\nOIL:\n\t\tOLIVE OIL, SOYBEAN OIL, SUNFLOWER OIL, COCONUT OIL.\n\nDAIRY PRODUCT:\n\t\tDARK CHOCOLATE.\n\nDRY FRUITS:\n\t\tPEANUTS.\n\nMUST:\n\t\tEGGPLANT (BRINJAL), OKRA (LADIES FINGER), BLUEBERRIES, BLACKBERRIES, BROWN RICE, CEREALS, BLACK BEANS, KIDNEY BEANS, CHICK PEAS, GREEN PEAS.\n\n\n\t* NOTE *\n\t\tQUIT SMOKING.\n ";
+    }
+
+    void diet2()
+    {
+        cout<<"\n\nOIL:\n\t\tOLIVE OIL, SOYBEAN OIL, SUNFLOWER OIL, COCONUT OIL.\n\nDAIRY PRODUCT:\n\t\tDARK CHOCOLATE.\n\nDRY FRUITS:\n\t\tALMONDS, WALNUTS, PEANUTS.\n\nMUST:\n\t\tEGGPLANT (BRINJAL), OKRA (LADIES FINGER), AVOCADO, APPLE, GRAPES, STRAWBERRY, BLUEBERRIES, BLACKBERRIES, OAT MEAL, OAT BRAN, BARLEY, BROWN RICE, SOY, MILLET, BLACK BEANS, KIDNEY BEANS, CHICK PEAS, GREEN PEAS.\n\nNON-VEG:\n\t\tSALMON, SHRIMP, SARDINES, ANCHORIES, MACKEREL, TROUT, HERRING.\n\n* NOTE *\n\t\tQUIT SMOKING.\n";
+    }
+
+    void diet3()
+    {
+        cout<<"\n\tFOLLOW YOUR CURRENT DIET.\n";
+    }
+
+    void diet4()
+    {
+        cout<<"\n\nOIL:\n\t\tOLIVE OIL, SOYBEAN OIL, SUNFLOWER OIL, COCONUT OIL.\n\nDAIRY PRODUCT:\n\t\tDARK CHOCOLATE.\n\nDRY FRUITS:\n\t\tALMONDS, WALNUTS, PEANUTS.\n\nMUST:\n\t\tEGGPLANT (BRINJAL), OKRA (LADIES FINGER), AVOCADO, APPLE, GRAPES, STRAWBERRY, OAT MEAL, OAT BRAN, BARLEY, BROWN RICE, SOY, MILLET, BLACK BEANS, KIDNEY BEANS, CHICK PEAS, GREEN PEAS.\n\nNON-VEG:\n\t\tSALMON, SHRIMP, SARDINES, ANCHORIES, MACKEREL, TROUT, HERRING.\n";
+    }
+
+    void tri_diet()
+    {
+        cout<<"\n\tOAT MEAL, OAT BRAN, APPLE, BANANA, ORANGE, PEAR, PRUNES, KIDNEY BEAN, LIMA BEAN, CHICK PEAS.\n\n* NOTE *\n\t\tAVOID EXCESS SUGAR.\n\t\tCONTROL WEIGHT.\n\t\tDO REGULAR EXCERCISE.\n";
+    }
 };
 
 int main()
 {
+    int a,b,c;
     cout<<"\n\n\t\t\t---------------------Welcome to your Diet Planner---------------------"<<endl;
     cout<<"\t\t\t(kindly enter in digits form only)"<<endl;
     report_data p1;
     p1.in_age();
     p1.data();
-    cout<<"\n\t\t\t------------------------------------------------------------------------"<<endl;
-    p1.test_tri();
-    p1.test_hdl();
-    p1.test_ldl();
-    p1.inc_satfat();
+    cout<<"\n--------------------------------------------------------------------------------------------"<<endl;
+    c = p1.test_tri();
+    b = p1.test_ldl();
+    a = p1.test_hdl();
+
     cout<<"\n\n\n\n\t\t\tThank you for entering your data it is under processing"<<endl;
     cout<<"\t\t\tGetting your diet ready..."<<endl;
+
+    if(a==0 && b==0)
+    p1.diet1();
+    else if(a==0 && b==1)
+    p1.diet2();
+    else if(a==1 && b==0)
+    p1.diet3();
+    else if(a==1 && b==1)
+    p1.diet4();
+    if(c==1)
+    p1.tri_diet();
+
     return 0;
+}
+
 }
 
 
